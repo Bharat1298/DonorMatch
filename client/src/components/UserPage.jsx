fc5fb3bad2b741b9baa6e0cddf1c0b7e789518e9
Bharat <BharatKhadka1298@gmail.com>
2024-09-14 17:00:21 -0400

    Creating User page, need Mongoose

--- client/src/components/UserPage.jsx
+++ client/src/components/UserPage.jsx
@@ -1,25 +1,4 @@
-import { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-
-const Charities = (props) => {
-    {props.charities.needs.map}
-}
-
-useEffect(() => {
-    async function getCharities() {
-      const response = await fetch(`http://localhost:5050/charities/`);
-      if (!response.ok) {
-        const message = `An error occurred: ${response.statusText}`;
-        console.error(message);
-        return;
-      }
-      const charities = await response.json();
-      setCharities(charities);
-    }
-    getCharities();
-    return;
-  }, [charities.length]);
+import db from "../../../server/db/connection";
 
   export default function(){
-    
   }
